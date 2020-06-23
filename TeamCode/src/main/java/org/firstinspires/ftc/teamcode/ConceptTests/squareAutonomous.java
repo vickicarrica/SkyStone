b3405b21f7c218f12b0be7e740a76583fd9a520e
vickicarrica <66328989+vickicarrica@users.noreply.github.com>
2020-06-23 13:39:03 -0500

    Update squareAutonomous.java

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/ConceptTests/squareAutonomous.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/ConceptTests/squareAutonomous.java
@@ -3,6 +3,7 @@ package org.firstinspires.ftc.teamcode.ConceptTests;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
 
 //Basic autonomous that causes a robot to move in a square
 
@@ -11,17 +12,23 @@ import com.qualcomm.robotcore.hardware.DcMotor;
 public class squareAutonomous extends LinearOpMode {
 
     //Define DcMotor variables
-    DcMotor leftMotor;
-    DcMotor rightMotor;
+    DcMotor leftFrontMotor;
+    DcMotor rightFrontMotor;
+    DcMotor leftBackMotor;
+    DcMotor rightBackMotor;
 
     @Override
     public void runOpMode(){
         //Pairs each DcMotor in the code with its corresponding motor port in the robot
-        leftMotor = hardwareMap.dcMotor.get("left_motor");
-        rightMotor = hardwareMap.dcMotor.get("right_motor");
+        leftFrontMotor = hardwareMap.dcMotor.get("left_front_motor");
+        rightBackMotor = hardwareMap.dcMotor.get("right_front_motor");
+        leftBackMotor = hardwareMap.dcMotor.get("left_back_motor");
+        rightBackMotor = hardwareMap.dcMotor.get("right_back_motor");
 
         //"Most robots need the motor on one side to be reversed to drive forward" (FTC BasicOpMode_Linear)
-        leftMotor.setDirection(DcMotor.Direction.REVERSE);
+        //sets both left motors (front&back) reversed
+        leftFrontMotor.setDirection(DcMotor.Direction.REVERSE);
+        leftBackMotor.setDirection(DcMotor.Direction.REVERSE);
 
         //Wait for start:
         waitForStart();
@@ -30,13 +37,17 @@ public class squareAutonomous extends LinearOpMode {
         for (int i = 0; i < 4; i ++)
         {
             //motors move at 25% for 3 seconds:
-            leftMotor.setPower(.25);
-            rightMotor.setPower(.25);
+            leftFrontMotor.setPower(.25);
+            rightFrontMotor.setPower(.25);
+            leftBackMotor.setPower(.25);
+            rightBackMotor.setPower(.25);
             sleep(3000);
 
             //stops motors:
-            leftMotor.setPower(0.0);
-            rightMotor.setPower(0.0);
+            leftFrontMotor.setPower(0.0);
+            rightFrontMotor.setPower(0.0);
+            leftBackMotor.setPower(0.0);
+            rightBackMotor.setPower(0.0);
 
             //waits for 1 sec until next command:
             sleep(1000);
@@ -44,13 +55,18 @@ public class squareAutonomous extends LinearOpMode {
             if (i < 3) //will not turn after the final side
             {
                 //turns (motors go in the opposite direction):
-                leftMotor.setPower(.25);
-                rightMotor.setPower(-.25);
+                leftFrontMotor.setPower(.25);
+                rightFrontMotor.setPower(-.25);
+                leftBackMotor.setPower(.25);
+                rightBackMotor.setPower(-.25);
+
                 sleep(2000); //guess, will need to be adjusted depending if it turned to little or too much
 
                 //stop motors:
-                leftMotor.setPower(0.0);
-                rightMotor.setPower(0.0);
+                leftFrontMotor.setPower(0.0);
+                rightFrontMotor.setPower(0.0);
+                leftBackMotor.setPower(0.0);
+                rightBackMotor.setPower(0.0);
 
                 //waits for 1 sec until next command:
                 sleep(1000);
